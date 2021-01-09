abf41ec4c9282108c1ec5421e710d6904ad4c416
Abdul Rahman Farachi <67346394+abdulrsf@users.noreply.github.com>
2021-01-10 03:04:36 +0400

    Delete cursor.js

--- cursor.js
+++
@@ -1,9 +0,0 @@
-let mouseCursor = document.querySelector(".custom-cursor");
-let navLinks = document.querySelectorAll(".nav-items li");
-
-window.addEventListener("mousemove", cursor);
-
-function cursor(e){
-    mouseCursor.style.top = e.pageY + "px";
-    mouseCursor.style.left = e.pageX + "px";
-}
